9f8071cf227f233f80fd5e4941f760e718ec0e73
Massimo Pavoni <maspavoni@gmail.com>
2024-03-16 13:17:15 +0100

    No more config.h + dunst notifications for volume and brightness

--- src/config.def.h
+++ src/config.def.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
+static const unsigned int gappx     = 4;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -12,8 +12,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -36,7 +36,9 @@ static const char *const autostart[] = {
 	"dunst", NULL,
 	"numlockx", NULL,
 	"xss-lock", "--", "slock", NULL,
-	NULL /* terminate */
+	"setxkbmap", "-layout", "us,it", "-option", "grp:win_space_toggle", NULL,
+	"/home/damax/.fehbg", NULL,
+        NULL /* terminate */
 };
 
 /* tagging */
@@ -52,7 +54,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -87,19 +89,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *slockcmd[] = { "slock", NULL };
-/* volume control */
-static const char *volmutecmd[] = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
-static const char *voldowncmd[] = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
-static const char *volupcmd[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
 /* media control */
 static const char *medplaypausecmd[] = { "playerctl", "play-pause", NULL };
 static const char *mednextcmd[] = { "playerctl", "next", NULL };
 static const char *medprevcmd[] = { "playerctl", "previous", NULL };
-/* brightness control */
-static const char *dimmercmd[]   = { "brightnessctl", "set", "5%-", NULL };
-static const char *brightercmd[] = { "brightnessctl", "set", "5%+", NULL };
+/* other */
+static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -137,14 +134,15 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,     XK_BackSpace,      quit,           {0} },
 	{ MODKEY|ControlMask|ShiftMask, XK_l,      spawn,          {.v = slockcmd } },
-	{ 0,                XF86XK_AudioMute,      spawn,          {.v = volmutecmd } },
-	{ 0,         XF86XK_AudioLowerVolume,      spawn,          {.v = voldowncmd } },
-	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          {.v = volupcmd } },
+	{ 0,                XF86XK_AudioMute,      spawn,          SHCMD("~/.local/bin/dunst-volume toggle") },
+	{ 0,         XF86XK_AudioLowerVolume,      spawn,          SHCMD("~/.local/bin/dunst-volume -5%") },
+	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          SHCMD("~/.local/bin/dunst-volume +5%") },
 	{ 0,                XF86XK_AudioPlay,      spawn,          {.v = medplaypausecmd } },
 	{ 0,                XF86XK_AudioNext,      spawn,          {.v = mednextcmd } },
 	{ 0,                XF86XK_AudioPrev,      spawn,          {.v = medprevcmd } },
-	{ 0,        XF86XK_MonBrightnessDown,      spawn,          {.v = dimmercmd } },
-	{ 0,          XF86XK_MonBrightnessUp,      spawn,          {.v = brightercmd } },
+	{ 0,        XF86XK_MonBrightnessDown,      spawn,          SHCMD("~/.local/bin/dunst-brightness 5%-") },
+	{ 0,          XF86XK_MonBrightnessUp,      spawn,          SHCMD("~/.local/bin/dunst-brightness 5%+") },
+	{ METAKEY|ShiftMask,            XK_s,      spawn,          {.v = screenshotcmd } },
 };
 
 /* button definitions */
