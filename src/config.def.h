62e4ed98048e117c41bb8ddec08020befcd42abc
Massimo Pavoni <maspavoni@gmail.com>
2024-03-20 12:39:56 +0100

    Actually use mono nerd symbols

--- src/config.def.h
+++ src/config.def.h
@@ -14,7 +14,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 4;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "FiraCode:size=13", "SymbolsNerdFont:size=13" };
+static const char *fonts[]          = { "FiraCode:size=13", "SymbolsNerdFontMono:size=13" };
 static const char dmenufont[]       = "FiraCode:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#555555";
