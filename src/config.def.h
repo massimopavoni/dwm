a8069118e7325c9cedde0f0bb1e17535f456caa9
Massimo Pavoni <maspavoni@gmail.com>
2024-03-26 18:32:48 +0100

    power dmenu keybind

--- src/config.def.h
+++ src/config.def.h
@@ -97,60 +97,62 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *slockcmd[] = { "slock", NULL };
+static const char *powermenucmd[] = { "dmenu_power", "-m", dmenumon, NULL };
 /* media control */
 static const char *medplaypausecmd[] = { "playerctl", "play-pause", NULL };
 static const char *mednextcmd[] = { "playerctl", "next", NULL };
 static const char *medprevcmd[] = { "playerctl", "previous", NULL };
 /* other */
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
-static const char *qalccmd[] = { "dmenu", "-C", "-l", "1", NULL };
+static const char *qalccmd[] = { "dmenu", "-m", dmenumon, "-C", "-l", "1", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	STACKKEYS(MODKEY,                          focus)
-	STACKKEYS(MODKEY|ShiftMask,                push)
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,     XK_BackSpace,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_l,      spawn,          {.v = slockcmd } },
-	{ 0,                XF86XK_AudioMute,      spawn,          SHCMD("~/.local/bin/dunst-volume toggle") },
-	{ 0,         XF86XK_AudioLowerVolume,      spawn,          SHCMD("~/.local/bin/dunst-volume -5%") },
-	{ 0,         XF86XK_AudioRaiseVolume,      spawn,          SHCMD("~/.local/bin/dunst-volume +5%") },
-	{ 0,                XF86XK_AudioPlay,      spawn,          {.v = medplaypausecmd } },
-	{ 0,                XF86XK_AudioNext,      spawn,          {.v = mednextcmd } },
-	{ 0,                XF86XK_AudioPrev,      spawn,          {.v = medprevcmd } },
-	{ 0,        XF86XK_MonBrightnessDown,      spawn,          SHCMD("~/.local/bin/dunst-brightness 5%-") },
-	{ 0,          XF86XK_MonBrightnessUp,      spawn,          SHCMD("~/.local/bin/dunst-brightness 5%+") },
-	{ METAKEY|ShiftMask,            XK_s,      spawn,          {.v = screenshotcmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = qalccmd } },
+	/* modifier                     key                       function        argument */
+	{ MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,                     togglebar,      {0} },
+	STACKKEYS(MODKEY,                                         focus)
+	STACKKEYS(MODKEY|ShiftMask,                               push)
+	{ MODKEY,                       XK_i,                     incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,                     setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return,                zoom,           {0} },
+	{ MODKEY,                       XK_Tab,                   view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,                     killclient,     {0} },
+	{ MODKEY,                       XK_t,                     setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,                     setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,                     setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,                 setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                 togglefloating, {0} },
+	{ MODKEY,                       XK_0,                     view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                     tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                                     0)
+	TAGKEYS(                        XK_2,                                     1)
+	TAGKEYS(                        XK_3,                                     2)
+	TAGKEYS(                        XK_4,                                     3)
+	TAGKEYS(                        XK_5,                                     4)
+	TAGKEYS(                        XK_6,                                     5)
+	TAGKEYS(                        XK_7,                                     6)
+	TAGKEYS(                        XK_8,                                     7)
+	TAGKEYS(                        XK_9,                                     8)
+	{ MODKEY|ShiftMask,             XK_BackSpace,             quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_l,                     spawn,          {.v = slockcmd } },
+	{ MODKEY|ControlMask|ShiftMask, XK_BackSpace,             spawn,          {.v = powermenucmd } },
+	{ 0,                            XF86XK_AudioMute,         spawn,          SHCMD("~/.local/bin/dunst-volume toggle") },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,          SHCMD("~/.local/bin/dunst-volume -5%") },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,          SHCMD("~/.local/bin/dunst-volume +5%") },
+	{ 0,                            XF86XK_AudioPlay,         spawn,          {.v = medplaypausecmd } },
+	{ 0,                            XF86XK_AudioNext,         spawn,          {.v = mednextcmd } },
+	{ 0,                            XF86XK_AudioPrev,         spawn,          {.v = medprevcmd } },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,          SHCMD("~/.local/bin/dunst-brightness 5%-") },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,          SHCMD("~/.local/bin/dunst-brightness 5%+") },
+	{ METAKEY|ShiftMask,            XK_s,                     spawn,          {.v = screenshotcmd } },
+	{ MODKEY|ShiftMask,             XK_p,                     spawn,          {.v = qalccmd } },
 };
 
 /* button definitions */
