2711f0e089e831b2ed37a220ebd8fbe6953944b0
Massimo Pavoni <maspavoni@gmail.com>
2025-04-11 00:02:47 +0200

    fix crashing x for conflicting keys

--- src/config.def.h
+++ src/config.def.h
@@ -84,8 +84,8 @@ static const Layout layouts[] = {
 	{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
 	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
 	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
-	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
-	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
+	{ MOD, XK_x,     ACTION##stack, {.i = 2 } }, \
+	{ MOD, XK_c,     ACTION##stack, {.i = -1 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
